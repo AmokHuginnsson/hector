d21f5956b0cdec068614abd7a1183c2522548473
Marcin Konarski <marcin.konarski@codestation.org>
2008-03-31 10:33:04 +0000

    r2788: PoC.

--- app/codestation/processor.cxx
+++ app/codestation/processor.cxx
@@ -16,23 +16,44 @@ namespace hector
 
 void application_processor( HApplication& app, ORequest& req )
 	{
-	HString pageName;
-	if ( req.lookup( "page", pageName ) )
-		pageName = "info";
-	HXml::HNodeProxy page = app.f_oDOM.get_element_by_id( "page" );
-	for ( HXml::HIterator it = page.begin(); it != page.end(); )
+	HString action;
+	if ( ! req.lookup( "action", action ) && ( action == "ajax" ) )
 		{
-		HXml::HNode::properties_t::iterator classIt = (*it).properties().find( "class" );
-		if ( ( classIt != (*it).properties().end() ) && ( classIt->second == "content" ) )
+		app.f_oDOM.clear();
+		app.f_oDOM.create_root( "xml" );
+		HXml::HNodeProxy root = app.f_oDOM.get_root();
+		root.add_node( HXml::HNode::TYPE::D_CONTENT, "Hello world!, query: " );
+		HString query;
+		if ( ! req.lookup( "query", query ) )
+			(*root.add_node( HXml::HNode::TYPE::D_NODE, "b" )).add_node( HXml::HNode::TYPE::D_CONTENT, query );;
+		}
+	else
+		{
+		HString pageName;
+		if ( req.lookup( "page", pageName ) )
+			pageName = "info";
+		HXml::HNodeProxy page = app.f_oDOM.get_element_by_id( "page" );
+		HXml waste;
+		waste.create_root( "x" );
+		HXml::HNodeProxy dummy = waste.get_root();
+		for ( HXml::HIterator it = page.begin(); it != page.end(); )
 			{
-			HXml::HNode::properties_t::iterator idIt = (*it).properties().find( "id" );
-			if ( ( idIt != (*it).properties().end() ) && ( idIt->second != pageName ) )
-				it = page.remove_node( it );
+			HXml::HNode::properties_t::iterator classIt = (*it).properties().find( "class" );
+			if ( ( classIt != (*it).properties().end() ) && ( classIt->second == "content" ) )
+				{
+				HXml::HNode::properties_t::iterator idIt = (*it).properties().find( "id" );
+				if ( ( idIt != (*it).properties().end() ) && ( idIt->second != pageName ) )
+					{
+					HXml::HIterator del = it;
+					++ it;
+					dummy.move_node( *del );
+					}
+				else
+					++ it;
+				}
 			else
 				++ it;
 			}
-		else
-			++ it;
 		}
 	}
 
