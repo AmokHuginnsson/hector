e9a9da982cbc7f9bcd9c9d5dfe58247d81e09ba5
Marcin Konarski <marcin.konarski@codestation.org>
2008-11-04 21:49:11 +0100

    deployment fixed

--- _deploy/jail-hectord.py
+++ _deploy/jail-hectord.py
@@ -1,5 +1,5 @@
 doNotCopy=["/home"]
-forceCopy=["/usr/bin/env","/lib/ld-linux.so.2","/lib/ld-linux-x86-64.so.2","/lib/ld-2.8.90.so","/lib/tfurca-makejail-to-pizda"]
+forceCopy=["/usr/bin/env","/lib/ld-linux.so.2","/lib/ld-linux-x86-64.so.2","/lib/ld-2.8.90.so","/usr/lib/libsqlite3.so","/lib/tfurca-makejail-to-pizda"]
 chroot="/home/hector"
 testCommandsInsideJail=["/bin/sh -c 'cd /nest;/usr/bin/env HOME=/nest NEST=/ LD_LIBRARY_PATH=/nest/lib /nest/1exec'"]
 processNames=["1exec"]
