b5ac6d9950d351adafd73a2b54228b3c3b202e4d
Marcin Konarski <marcin.konarski@codestation.org>
2011-08-04 16:17:01 +0200

    deployments scripts update.

--- _deploy/jail-hectord.py
+++ _deploy/jail-hectord.py
@@ -1,7 +1,7 @@
 doNotCopy=["/home"]
 forceCopy=["/usr/bin/env","/lib/ld-linux.so.2","/lib/ld-linux-x86-64.so.2","/lib/ld-2.8.90.so","/usr/lib/libsqlite3.so","/lib/tfurca-makejail-to-pizda"]
 chroot="/home/hector"
-testCommandsInsideJail=["/bin/sh -c 'cd /nest;/usr/bin/env HOME=/nest NEST=/ LD_LIBRARY_PATH=/nest/lib /nest/1exec'"]
+testCommandsInsideJail=["/bin/launchtool -u hector --chdir='/nest' -t hector-install \". /nest/set-limits.sh; /usr/bin/env HOME=/nest NEST=/ LD_LIBRARY_PATH=/nest/lib /nest/1exec\""]
 processNames=["1exec"]
 cleanJailFirst=0
 #sleepAfterStartCommand=2
